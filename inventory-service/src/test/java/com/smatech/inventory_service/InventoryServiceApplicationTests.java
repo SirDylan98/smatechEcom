fb48cfc24c819ec38bbdb7c7a7baae57750d7990
SirDylan98 <dylandzvenetashinga@gmail.com>
2025-02-07 14:20:57 +0200

    Project Setup first phase

---
+++ inventory-service/src/test/java/com/smatech/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smatech.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
